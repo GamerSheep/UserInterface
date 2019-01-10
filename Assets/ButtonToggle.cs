d2239ff12deb3e3a6176a0c3ff0097fd735c73a2
Andreas Styverbjörne <andreas.styverbjorne@elev.ga.lbs.se>
2019-01-10 10:55:27 +0100

    menus and buttons

---
+++ Assets/ButtonToggle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ButtonToggle : MonoBehaviour
+{
+    
+    public void ToggleButtons(bool state)
+    {
+        GetComponent<Button>().interactable = state;
+    }
+
+
+}
---
+++ Assets/RotateTriangle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTriangle : MonoBehaviour
+{
+
+    public void Rotate(float degrees)
+    {
+        transform.Rotate(0, 0, degrees);
+    }
+
+}
